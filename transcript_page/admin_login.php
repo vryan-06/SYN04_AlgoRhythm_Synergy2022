1c3f24e43494fb2c46e28c44fb117bcf8836a64c
V Ryan <ryan19vt@gmail.com>
2022-10-22 18:36:14 +0530

    Updated with transcription db

--- transcript_page/admin_login.php
+++ transcript_page/admin_login.php
@@ -1,56 +1,54 @@
 <?php
 
-//user_login.php
+//admin_login.php
+
 
 include 'database_connection.php';
 
 include 'function.php';
 
-if(is_user_login())
-{
-	header('location:issue_book_details.php');
-}
 
 $message = '';
 
 if(isset($_POST["login_button"]))
 {
+
 	$formdata = array();
 
-	if(empty($_POST["user_email_address"]))
+	if(empty($_POST["admin_email"]))
 	{
 		$message .= '<li>Email Address is required</li>';
 	}
 	else
 	{
-		if(!filter_var($_POST["user_email_address"], FILTER_VALIDATE_EMAIL))
+		if(!filter_var($_POST["admin_email"], FILTER_VALIDATE_EMAIL))
 		{
 			$message .= '<li>Invalid Email Address</li>';
 		}
 		else
 		{
-			$formdata['user_email_address'] = trim($_POST['user_email_address']);
+			$formdata['admin_email'] = $_POST['admin_email'];
 		}
 	}
 
-	if(empty($_POST['user_password']))
+	if(empty($_POST['admin_password']))
 	{
 		$message .= '<li>Password is required</li>';
-	}	
+	}
 	else
 	{
-		$formdata['user_password'] = trim($_POST['user_password']);
+		$formdata['admin_password'] = $_POST['admin_password'];
 	}
 
 	if($message == '')
 	{
 		$data = array(
-			':user_email_address'		=>	$formdata['user_email_address']
+			':admin_email'		=>	$formdata['admin_email']
 		);
 
 		$query = "
-		SELECT * FROM lms_user 
-        WHERE user_email_address = :user_email_address
+		SELECT * FROM lms_admin 
+        WHERE admin_email = :admin_email
 		";
 
 		$statement = $connect->prepare($query);
@@ -61,67 +59,80 @@ if(isset($_POST["login_button"]))
 		{
 			foreach($statement->fetchAll() as $row)
 			{
-				if($row['user_status'] == 'Enable')
+				if($row['admin_password'] == $formdata['admin_password'])
 				{
-					if($row['user_password'] == $formdata['user_password'])
-					{
-						$_SESSION['user_id'] = $row['user_unique_id'];
-						header('location:issue_book_details.php');
-					}
-					else
-					{
-						$message = '<li>Wrong Password</li>';
-					}
+					$_SESSION['admin_id'] = $row['admin_id'];
+
+					header('location:admin/index.php');
 				}
 				else
 				{
-					$message = '<li>Your Account has been disabled</li>';	
+					$message = '<li>Wrong Password</li>';
 				}
 			}
-		}
+		}	
 		else
 		{
 			$message = '<li>Wrong Email Address</li>';
 		}
 	}
+
 }
 
 include 'header.php';
 
 ?>
 
-<div class="d-flex align-items-center justify-content-center" style="height:700px;">
-	<div class="col-md-6">
-		<?php 
+<div class="d-flex align-items-center justify-content-center" style="min-height:700px;">
 
+	<div class="col-md-6">
+
+		<?php 
 		if($message != '')
 		{
 			echo '<div class="alert alert-danger"><ul>'.$message.'</ul></div>';
 		}
-
 		?>
+
 		<div class="card">
-			<div class="card-header">User Login</div>
+
+			<div class="card-header">Admin Login</div>
+
 			<div class="card-body">
+
 				<form method="POST">
+
 					<div class="mb-3">
 						<label class="form-label">Email address</label>
-						<input type="text" name="user_email_address" id="user_email_address" class="form-control" />
+
+						<input type="text" name="admin_email" id="admin_email" class="form-control" />
+
 					</div>
+
 					<div class="mb-3">
 						<label class="form-label">Password</label>
-						<input type="password" name="user_password" id="user_password" class="form-control" />
+
+						<input type="password" name="admin_password" id="admin_password" class="form-control" />
+
 					</div>
+
 					<div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+
 						<input type="submit" name="login_button" class="btn btn-primary" value="Login" />
+
 					</div>
+
 				</form>
+
 			</div>
+
 		</div>
+
 	</div>
+
 </div>
 
-<?php 
+<?php
 
 include 'footer.php';
 
