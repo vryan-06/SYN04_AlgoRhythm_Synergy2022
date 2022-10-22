009c0ac6fdd3c79728c1e04d58b4adfd68b45960
Hisbaansay <99158586+Hisbaansay@users.noreply.github.com>
2022-10-22 14:26:26 +0530

    issue_application updated

--- transcript_page/issue_application.php
+++ transcript_page/issue_application.php
@@ -1,7 +1,49 @@
 <?php
     include 'header.php';
 ?>
+<div class="card">
+            <div class="card-header">Transcript Details</div>
+			<div class="card-body">
+                <form method="POST" enctype="multipart/form-data">
+                    <div class="mb-3">
+                            <label class="form-label">Student Roll No </label>
+                            <input type="text" name="roll_no" id="roll_no" class="form-control" />
+                        </div>
+                       
+                        <div class="mb-3">
+                            <label class="form-label">First Name</label>
+                            <input type="text" name="f_name" class="form-control" id="f_name" value="" />
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Last Name</label>
+                            <input type="text" name="l_name" id="l_name" class="form-control" />
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Branch</label>
+                            <input type="text" name="Branch_name" id="Branch_id" class="form-control" />
+                        </div>
+                        <div class="mb-4">
+                            <label class="form-label">Semester</label>
+                            <input type="text" name="Semester_name" id="semester_id" class="form-control" />
+                        </div>
+                        <div class="mb-5">
+                            <label class="form-label">CGPA</label>
+                            <input type="text" name="Cgpa_name" id="cgpa_id" class="form-control" />
+                        </div>
+                        <div class="mb-6">
+                            <label class="form-label">SGPA</label>
+                            <input type="SGPA" name="SGPA_name" id="SGPA_id" class="form-control" />
+                        </div>
 
+                        <div class="text-center mt-4 mb-2">
+						<input type="submit" name="register_button" class="btn btn-primary" value="Register" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php
     include 'footer.php';
 ?>
